ca95e9e09b6743a8e957b89491dc2f9ba997bede
Christian LaFrance <clafranc@talapas-ln1.uoregon.edu>
2022-09-07 17:45:50 -0700

    script to run fastqc and mean_phred.py

--- compare_qc.sh
+++ compare_qc.sh
@@ -6,10 +6,6 @@
 #SBATCH --account=bgmp      ### Account used for job submission
 #SBATCH --error=compare_qc.err          ### File in which to store job error messages
 
-file="29_4E_fox_S21_L008_R2_001.fastq.gz"
-
-echo $file
-
 conda activate QAA
 module load fastqc
 
@@ -21,7 +17,12 @@ f2="11_2H_both_S9_L008_R2_001.fastq.gz"
 f3="29_4E_fox_S21_L008_R1_001.fastq.gz"
 f4="29_4E_fox_S21_L008_R2_001.fastq.gz"
 
+/usr/bin/time -v ./mean_phred.py -f $dataDir/$f1 -l 101 -p $f1
+/usr/bin/time -v ./mean_phred.py -f $dataDir/$f2 -l 101 -p $f2
+/usr/bin/time -v ./mean_phred.py -f $dataDir/$f3 -l 101 -p $f3
+/usr/bin/time -v ./mean_phred.py -f $dataDir/$f4 -l 101 -p $f4
 
-/usr/bin/time -v ./mean_phred.py -f $dataDir/$file -l 101 -p "29_4E_fox_S21_L008_R2_001"
-
-/usr/bin/time -v fastqc $dataDir/$file -o $myDir/fastqc_output
+/usr/bin/time -v fastqc $dataDir/$f1 -o $myDir/fastqc_output
+/usr/bin/time -v fastqc $dataDir/$f2 -o $myDir/fastqc_output
+/usr/bin/time -v fastqc $dataDir/$f3 -o $myDir/fastqc_output
+/usr/bin/time -v fastqc $dataDir/$f4 -o $myDir/fastqc_output
